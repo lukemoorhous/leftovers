e0e5ae75eab635b82350b9e1a58cfa69c0b91b21
dylanburton <37320555+dylanburton@users.noreply.github.com>
2018-10-20 18:46:29 -0500

    Add files via upload

---
+++ planter/data/Field.java
@@ -0,0 +1,26 @@
+package planter.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Field {
+	private String fieldName = null;
+	private List<FieldRecord> records = null;
+	
+	public Field(String name) {
+		this.fieldName = name;
+		records = new ArrayList<FieldRecord>();
+	}
+	public String getFieldName() {
+		return fieldName;
+	}
+	public List<FieldRecord> records() {
+		return records;
+	}
+	public void setRecords(List<FieldRecord> records) {
+		this.records = records;
+	}
+	public void addRecord(FieldRecord record) {
+		this.records.add(record);
+	}
+}
---
+++ planter/data/FieldRecord.java
@@ -0,0 +1,45 @@
+package planter.data;
+
+public class FieldRecord {
+
+	private String fieldName = null;
+	private String crop = null;
+	private double water = 0;
+	private double height = 0;
+	
+	public FieldRecord(String fieldName) {
+		setFieldName(fieldName);
+		
+	}
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	
+	public String getFieldName() {
+		return this.fieldName;
+	}
+	
+	public String getCrop() {
+		return this.crop;
+	}
+	
+	public void setCrop(String crop) {
+		this.crop = crop;
+	}
+	
+	public double getWater() {
+		return this.water;	
+	}
+	
+	public void setWater(double water) {
+		this.water = water;
+	}
+	
+	public double getHeight() {
+		return this.height;
+	}
+	
+	public void setHeight(double height) {
+		this.height = height;
+	}
+}
